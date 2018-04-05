e2803a682a8a38ea2edaff7df6fb87e48c83df6e
Mitohku <34718938+Mitohku@users.noreply.github.com>
2018-04-05 11:26:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,6 +29,33 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+@bot.event()
+async def on_server_join(ctx, *, member : discord.Member = None):
+	member = member.mention
+	
+	embed = discord.Embed(colour = discord.Colour(0xA522B3))
+	embed.description = f"Welcome to the server **{member}**! It's nowhere near done and still being set up. You have __24 hours__ to say **`Hai`** or **`Bai`** to access to the server.\nAlso, please read pinned messages."
+	await ctx.send(embed = embed)
+
+@bot.event()
+async def on_message(ctx, *, message, member : discord.Member = None):
+	member = message.author
+	welcome = bot.get_channel(429820948485767168)
+	
+	if message == "Hai":
+		if message in welcome:
+			await ctx.send("Welcome!")
+			await asyncio.sleep(0.50)
+			await member.add_roles(*Member, reason="New to Server")
+		else:
+			await ctx.send("COMMAND ERROR (HAI)")
+	if message == "Bai":
+		if message in welcome:
+			await ctx.send("Kicking from server..")
+			await asyncio.sleep(0.50)
+			await member.add_roles(*kick, reason="Bai - No Access to Server")
+		else:
+			await ctx.send("COMMAND ERROR (BAI)")
 
 #########################################
 
@@ -198,36 +225,6 @@ async def prestiemilia(ctx):
 
 ##      GAME COMMAND (NOT WORKING)     ##
 
-#########################################
-
-@bot.event()
-async def on_server_join(ctx, *, member : discord.Member = None):
-	member = member.mention
-	
-	embed = discord.Embed(colour = discord.Colour(0xA522B3))
-	embed.description = f"Welcome to the server **{member}**! It's nowhere near done and still being set up. You have __24 hours__ to say **`Hai`** or **`Bai`** to access to the server.\nAlso, please read pinned messages."
-	await ctx.send(embed = embed)
-
-@bot.event()
-async def on_message(ctx, *, message, member : discord.Member = None):
-	member = message.author
-	welcome = bot.get_channel(429820948485767168)
-	
-	if message == "Hai":
-		if message in welcome:
-			await ctx.send("Welcome!")
-			await asyncio.sleep(0.50)
-			await member.add_roles(*Member, reason="New to Server")
-		else:
-			await ctx.send("COMMAND ERROR (HAI)")
-	if message == "Bai":
-		if message in welcome:
-			await ctx.send("Kicking from server..")
-			await asyncio.sleep(0.50)
-			await member.add_roles(*kick, reason="Bai - No Access to Server")
-		else:
-			await ctx.send("COMMAND ERROR (BAI)")
-			
 #########################################
 
 @bot.command(aliases = ['pfp'])
